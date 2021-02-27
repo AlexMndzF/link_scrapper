b4b815839826984705f2473e8eba7a98f9058988
Alex Méndez Fuentes <alexmendezfuentes@MacBook-Air-de-Alex.local>
2021-02-27 18:05:55 +0100

    CLN: refactorizacion código

--- Utils.py
+++ Utils.py
@@ -15,33 +15,29 @@ class Node:
 class HtmlParser:
     def __init__(self):
         self.regex = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
-        self._get_html()
-        self._get_links()
 
     def get_links(self, url):
         self.url = url
         self._get_html()
-        self._get_links()
+        links = set(re.findall(self.regex, self.soups))
+        return links
 
     def _get_html(self):
         res = requests.get(self.url)
         html = res.text
         self.soups = str(BeautifulSoup(html, 'html.parser'))
 
-    def _get_links(self):
-        self.links = set(re.findall(self.regex, self.soups))
 
 
 class GetUrls:
-    def __init__(self, HtmlParser, url):
-        self.htmlparser = HtmlParser
-        self.url = url
+    def __init__(self, htmlparser=HtmlParser()):
+        self.htmlparser = htmlparser
 
     def get_urls(self, url, deep, indentation):
         print("- " * indentation, url.value)
         if deep == 0:
             return
-        links = HtmlParser(self.url).links
+        links = self.htmlparser.get_links(url.value)
         for e in links:
             node = Node(e)
             self.get_urls(node, deep - 1, indentation + 1)
--- main.py
+++ main.py
@@ -18,27 +18,11 @@ def get_config():
     return args
 
 
-def get_urls(url, deep, indentation):
-    """
-    Funcion que devuelve una lista de urls contenidas en la url principal.
-    """
-
-    print("- "*indentation, url.value)
-
-    if deep == 0:
-        return
-    links = HtmlParser().links
-    for e in links:
-        node = Node(e)
-        get_urls(node, deep - 1, indentation + 1)
-        url.add_children(node)
-
-
 def main():
     args = 1  # get_config()
     url = Node("https://www.google.com")  # Nodo(args.url)
     deep = 2  # args.deep
-    get_urls(url, deep, 0)
+    GetUrls().get_urls(url=url, deep=deep, indentation=0)
 
 
 if __name__ == "__main__":
